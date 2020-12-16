7b679a4b46ba34211861f799c563b936f843a407
IrinaKom7 <irinka.kom89@gmail.com>
2020-12-16 22:55:47 +0300

    first commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('URL', 'http://localhost/practice_18/task_18.7_2mp/'); 
+define('UPLOAD_MAX_SIZE', 1000000); 
+define('ALLOWED_TYPES', ['image/jpeg', 'image/png', 'image/gif']);
+define('UPLOAD_DIR', 'gallery');
+define('COMMENT_DIR', 'comments');
+?>
